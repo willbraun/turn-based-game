b3b2cb655a3dc62a9b21c0decd04975039b9a9fe
Levi Shevick <levidshevick@gmail.com>
2022-05-26 18:59:52 -0400

    adds basic event listener to select button

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,23 @@ import { Game, Hero, Enemy } from './classes.js';
 
 // EDIT SELECTORS
 
+let hero, enemy;
+
 // prompt user to select hero
 document.querySelector('.hero1button').addEventListener('click', () => {
     const hero = new Hero('kid1', 100)
 });
+const buttonOne = document.querySelector('.select-hero-one');
+const buttonTwo = doucment.querySelector('.select-hero-two');
+const buttonThree = document.querySelector('.select-hero-three');
+const startScreen = document.querySelector('.opening-screen');
 
+buttonOne.addEventListener('click', () => {
+    hero = new Hero('kid1', 100);
+    enemy = new Enemy('bully1', 100);
+    
+
+})
 
 
 // autogenerate enemy randomly
