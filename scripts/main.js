bdc5aa26e2b68edc55d9b31aa87fa05bbe762575
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-05-26 17:08:26 -0400

    added test hero and enemy

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import { Game, Hero, Enemy } from './classes.js';
 
-// EDIT SELECTORS
+let hero = {name: 'mario', health: 100};
+let enemy = {name: 'bowser', health: 100};
 
 // prompt user to select hero
 document.querySelector('.hero1button').addEventListener('click', () => {
