d33c6c3aa216c148ee48ddd224391640a7d7dc17
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-05-27 16:38:50 -0400

    made event listener setting more dry

--- scripts/main.js
+++ scripts/main.js
@@ -9,19 +9,12 @@ const selectWill = document.querySelector('.select-hero-one');
 const selectMatt = document.querySelector('.select-hero-two');
 const selectLevi = document.querySelector('.select-hero-three');
 
-selectWill.addEventListener('click', () => {
-    gameHero = generateHero(heroes[0]);
-    setUpGame(gameHero, gameEnemy);
+[selectWill, selectMatt, selectLevi].forEach((button, i) => {
+    button.addEventListener('click', () => {
+        gameHero = generateHero(heroes[i]);
+        setUpGame(gameHero, gameEnemy);
+    });
 });
-selectMatt.addEventListener('click', () => {
-    gameHero = generateHero(heroes[1]);
-    setUpGame(gameHero, gameEnemy);
-});
-selectLevi.addEventListener('click', () => {
-    gameHero = generateHero(heroes[2]);
-    setUpGame(gameHero, gameEnemy);
-});
-
 
 // testing
 // let testHero = new Hero({name: 'test', health: 100, powerLevel: 1.5});
